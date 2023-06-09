92b4c9d89f442452149306e556fd7ad692c9c9a0
Semih Arslan <semihsemih@me.com>
2023-06-09 14:39:32 +0300

    Change app port

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000. Go to http://localhost:3000/')
+server.listen(80, () => {
+    console.log('Server is running on port 80. Go to http://localhost/')
 });
